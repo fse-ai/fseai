1e562331c18784860ff1212914979bae00476266
Hanoona Rasheed <hanoona@uniqueroboticsedu.com>
2020-10-31 06:25:53 -0700

    Update setup classifiers, and email id

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="fseai",
     version="0.0.1",
     author="Hanoona Rasheed",
-    author_email="hanoona@uniqueroboticsedu.com",
+    author_email="hanoona.rasheed@inkerrobotics.com",
     description="Full Stack Engineering AI course package",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,18 +19,18 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Education",
-        "License :: OSI Approved :: Apache License, Version 2.0 (Apache-2.0)",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Operating System :: Microsoft :: Windows :: Windows 10"
-        "Environment :: Win32 (MS Windows)"
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Environment :: Win32 (MS Windows)",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",       
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.6',
 )
